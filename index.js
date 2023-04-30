b2fce1ca8d82c1ba09741e65365f0c546e889184
= <=>
2023-04-30 17:57:38 +0530

    code commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function changeMode(){
+    let mybody=document.body;
+    mybody.classList.toggle('darkmode')
+    if(mybody.classList.contains('darkmode')){
+        document.getElementById("icon").src="moon.png";
+    }
+    else{
+        document.getElementById("icon").src="sun.png";
+    }
+}
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+    document.getElementsByTagName('body').style.opacity='0.7'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+    document.getElementsByTagName('body').style.opacity='1'
+}
